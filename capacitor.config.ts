0d1d1b09461a3546ac82fbd7f395e3b7edabb776
nightisyang <shenyangc86@gmail.com>
2022-12-16 12:19:40 +0800

    android capacitor set up

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.splish.ionicangularrecipe',
+  appName: 'ionic-angular-recipe',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
